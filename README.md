364400ba6b13b34b4deb9b26e786de5a7bba7e10
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-10-24 15:12:11 -0700

    finish text analyzer reading, begin practice
    Co-authored-by: Noah Kise <noah@kisefamily.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-####Describe: wordCounter()
+#### Describe: wordCounter()
 
 Test: "It should return 1 if a passage has just one word."
 Code:
@@ -20,7 +20,7 @@ Test: "It should return 0 for a string that is only spaces."
 Code: wordCounter("            ");
 Expected Output: 0
 
-####Describe: numberOfOccurrencesInText()
+#### Describe: numberOfOccurrencesInText()
 
 Test: "It should return 0 occurrences of a word for an empty string."
 Code:
@@ -56,3 +56,26 @@ const text = "Red! Red. I like red, green, and yellow.";
 const word = "Red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 3
+
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
+
+#### Describe: boldPassage()
+
+Test: "It should return null if no word or text is entered."
+Code:
+const text = "";
+const word = "";
+boldPassage(word, text);
+Expected Output: null
+
+Test: "It should return a non-matching word in a p tag."
+Code:
+const word = "hello";
+const text = "yo";
+boldPassage(word, text);
+Expected Output: <p>yo</p>
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+// Utility Logic
+
+function isEmpty(testString) {
+    return (testString.trim().length === 0);
+}
+
 // Business Logic
 
 function wordCounter(text) {
-    if (text.trim().length === 0) {
+    if (isEmpty(text)) {
         return 0;
     }
     let wordCount = 0;
@@ -15,9 +21,12 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
+    if (isEmpty(word)) {
+        return 0;
+    }
     const textArray = text.split(" ");
     let wordCount = 0;
-    textArray.forEach(function(element) {
+    textArray.forEach(function (element) {
         if (element.toUpperCase().includes(word.toUpperCase())) {
             wordCount++
         }
@@ -28,7 +37,7 @@ function numberOfOccurrencesInText(word, text) {
 function censor(text) {
     const textArray = text.split(" ");
     let cleanText = ""
-    textArray.forEach(function(element) {
+    textArray.forEach(function (element) {
         if (element.toUpperCase().includes("ZOINKS") || element.toUpperCase().includes("MUPPETEER") || element.toUpperCase().includes("BIFFARONI") || element.toUpperCase().includes("LOOPDALOOP")) {
             const beep = "*"
             cleanText += element.charAt(0) + beep.repeat(element.length - 1) + " ";
@@ -36,3 +45,46 @@ function censor(text) {
     });
     return cleanText.trim();
 }
+
+function boldPassage(word, text) {
+    if (isEmpty(word) || isEmpty(text)) {
+        return null;
+    }
+    const p = document.createElement("p");
+    const textArray = text.split(" ");
+    textArray.forEach(function (element, index) {
+        if (element === word) {
+            const bold = document.createElement("strong");
+            bold.append(element);
+            p.append(bold);
+        } else {
+            p.append(element);
+        }
+        if (index !== (textArray.length - 1)) {
+            p.append(" ");
+        }
+    });
+    return p;
+}
+
+// UI Logic
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const passage = censor(document.getElementById("text-passage").value);
+    const word = document.getElementById("word").value;
+    const wordCount = wordCounter(passage);
+    const occurencesOfWord = numberOfOccurrencesInText(word, passage);
+    document.getElementById("total-count").innerText = wordCount;
+    document.getElementById("selected-count").innerText = occurencesOfWord;
+    let boldedPassage = boldPassage(word, passage);
+    if (boldedPassage) {
+        document.querySelector("div#bolded-passage").append(boldedPassage);
+    } else {
+        document.querySelector("div#bolded-passage").innerText = null;
+    }
+}
+
+window.addEventListener("load", function () {
+    document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
